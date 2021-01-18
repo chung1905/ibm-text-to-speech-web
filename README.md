b6146abd0202028f126548bc223fbc8b0e7ce6f3
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2021-01-18 13:04:01 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# IBM Text to Speech web interface
+
+## Setup
+- PHP
+- Get credentials
+  - Sign up IBM Cloud account: https://cloud.ibm.com/
+  - Create "Text to Speech" service (free or paid): https://cloud.ibm.com/catalog/services/text-to-speech
+  - Get `API Key` and `URL`
+- Add credentials to environment
+  - Copy `config.php.sample` to `config.php`: ```cp config.php.sample config.php```
+  - Edit `API_KEY` and `API_URL` as your service
+- Done
---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+$voices = [
+    'en-AU_CraigVoice',
+    'en-AU_MadisonVoice',
+    'en-GB_CharlotteV3Voice',
+    'en-GB_JamesV3Voice',
+    'en-GB_KateVoice',
+    'en-GB_KateV3Voice',
+    'en-US_AllisonVoice',
+    'en-US_AllisonV3Voice',
+    'en-US_EmilyV3Voice',
+    'en-US_HenryV3Voice',
+    'en-US_KevinV3Voice',
+    'en-US_LisaVoice',
+    'en-US_LisaV3Voice',
+    'en-US_MichaelVoice',
+    'en-US_MichaelV3Voice',
+    'en-US_OliviaV3Voice',
+];
+$defaultVoice = 'en-US_MichaelV3Voice';
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['text'])) {
+    $env = include_once 'config.php';
+    function genFileName(string $content): string
+    {
+        $content = preg_replace('/\W+/', ' ', $content);
+        $words = explode(' ', $content);
+        $words = array_slice($words, 0, 3);
+
+        return implode('-', $words) . '-' . time();
+    }
+
+    $voice = $defaultVoice;
+    if (isset($_POST['voice'])) {
+        if (in_array($_POST['voice'], $voices, true)) {
+            $voice = $_POST['voice'];
+        }
+    }
+    $apikey = $env['API_KEY'];
+    $url = $env['API_URL'];
+    $text = $_POST['text'];
+    $text = preg_replace("/\r\n+|\r+|\n+/", '. ', trim($text));
+    $text = str_replace('"', '\"', $text);
+    $fileName = genFileName($text);
+    $output = 'out/' . $fileName . '.wav';
+    $logFile = 'out/' . $fileName . '.log';
+    $curl = <<<CURL
+curl -X POST -u "apikey:${apikey}" \
+--header "Content-Type: application/json" \
+--header "Accept: audio/wav" \
+--data "{\"text\":\"$text\"}" \
+--output $output "${url}/v1/synthesize?voice=$voice"
+
+CURL;
+    file_put_contents($logFile, $curl);
+    exec($curl);
+    if (headers_sent()) {
+        die("<script type=\"text/javascript\">window.location='$output';</script>");
+    } else {
+        header('Location: ' . $output);
+        die();
+    }
+}
+?>
+<html lang="en">
+<head>
+    <title>TTS</title>
+</head>
+<body>
+<main>
+    <form action="" name="tts" method="post">
+        <div style="width: 80%; margin: 2% 10%;">
+            <div style="margin-bottom: 10px;">
+                <textarea name="text" style="width: 100%" rows="10"></textarea>
+            </div>
+            <select name="voice">
+                <option value="<?= $defaultVoice ?>" selected>Default: <?= $defaultVoice ?></option>
+                <?php foreach ($voices as $v) { ?>
+                    <option value="<?= $v ?>"><?= $v ?></option>
+                <?php } ?>
+            </select>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+</main>
+</body>
+</html>
